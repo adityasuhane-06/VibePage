5d1971e6dae771ade04fe877915888dfc40221f7
Aditya Suhane <adityasuhane01@gmail.com>
2025-07-26 19:36:20 +0530

    Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,10 @@ import SearchPage from './Pages/search.page.jsx'
 import PageNotFound from './Pages/404.page'
 import ProfilePage from './Pages/profile.page'
 import BlogPage from './Pages/blog.page'
-
+import { useSelector } from 'react-redux'
+import ProtectedRoute from './component/ProtectedRoute'
 function App() {
+  const {isLoggedIN, accessToken} = useSelector((state) => state.auth.data);
 
   return (
     <>
@@ -21,14 +23,27 @@ function App() {
       <Route index element={<HomePage/>}/>
       
       
-      <Route path ='/signup' element={<UserAuthForm/>} />
+      <Route path ='/signup'  element={<UserAuthForm/>} />
       <Route path ='/login' element={<Login/>} />
       <Route path ='/write' element={<Editor/>} />
       <Route  path='search/:query/' element={<SearchPage />} />
       
-      <Route path='/about' element={<ProfilePage/>} />
-      <Route path="user/:id" element={<ProfilePage/>} />
-      <Route path='/blog/:blog_id' element={<BlogPage/>} />
+      <Route path='/about' element={
+        <ProtectedRoute>
+        <ProfilePage/>
+        </ProtectedRoute>
+        } />
+      <Route path="user/:id" element={
+        <ProtectedRoute>
+          <ProfilePage />
+        </ProtectedRoute>
+        
+        } />
+      <Route path='/blog/:blog_id' element={
+        <ProtectedRoute>
+        <BlogPage/>
+        </ProtectedRoute>
+        } />
       <Route path='*' element={<PageNotFound />} />
       </Route>
       
---
+++ src/component/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const ProtectedRoute = ({ children }) => {
+  const { isLoggedIN, accessToken } = useSelector((state) => state.auth.data);
+  const location = useLocation();
+
+  // Check if user is authenticated
+  if (!isLoggedIN || !accessToken) {
+    // Redirect to login page with return url
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+export default ProtectedRoute;
